3920cb8815af64bdb26f5a17ec3bcc68c514e710
Ruben Taelman <ruben.taelman@ugent.be>
2016-03-25 10:41:26 +0100

    Correctly take into account NaN's

--- bin/assemble_max_csv.sh
+++ bin/assemble_max_csv.sh
@@ -4,13 +4,13 @@ for file in "$@"; do
     if [ -f $file ] && [ $(wc -l <$file) -gt 0 ]; then
         echo -n $(echo "$file" | sed "s/^.*dup=\([0-9]*\).*$/\1/")
         tail -n +2 $file | gawk -F',' 'BEGIN{\
-                for(i=1; i<=NF; i++){sum[i]="NaN";ok[i]=0}\
+                for(i=1; i<=NF; i++){max[i]="NaN"}\
             }\
             {\
-                for(i=1; i<=NF; i++){if($i!="NaN"){sum[i]+=$i;ok[i]=1}}\
+                for(i=1; i<=NF; i++){if($i>max[i]&&$i!="NaN")max[i]=$i}\
             }\
             END {\
-                for(i=1; i<=NF; i++){if(ok[i]){res=sum[i]/NR}else{res="NaN"};printf ",%s",res} printf "\n"\
+                for(i=1; i<=NF; i++){printf ",%s",max[i]} printf "\n"\
             }'
     fi
 done
--- bin/create_all_averaged.sh
+++ bin/create_all_averaged.sh
@@ -4,11 +4,11 @@ for f in 0.1 0.5; do
     for q in Q1 Q2 Q3 Q6 Q9 Q11; do
         for e in cqels csparql "querystreamer_type=graphs;interval=false;caching=false"; do
             files=""
-            for d in 1 5 10 20 50; do
+            for d in 1 20 50; do
                 files="$files $dir/$e/r=1.0;f=$f;dup=$d;q=[$q.txt].csv"
             done
-            outfile="$dir/$e/r=1.0;f=$f;dup=averaged;q=[$q.txt].csv"
-            ./assemble_averaged_csv.sh $files > $outfile
+            ./assemble_averaged_csv.sh $files > "$dir/$e/r=1.0;f=$f;dup=averaged;q=[$q.txt].csv"
+            ./assemble_max_csv.sh $files > "$dir/$e/r=1.0;f=$f;dup=max;q=[$q.txt].csv"
         done
     done
 done
