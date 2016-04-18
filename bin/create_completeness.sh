fdbd5379aa52bd97a939959514aba2b65c2f6380
Ruben Taelman <ruben.taelman@ugent.be>
2016-04-18 12:05:25 +0200

    Fix incorrect completeness calculation

--- bin/create_completeness.sh
+++ bin/create_completeness.sh
@@ -6,7 +6,7 @@ for f in 0.1 0.5; do
         echo 'q,querystreamer,csparql,cqels' > $outfile
         for q in Q1 Q2 Q3 Q6 Q9 Q11; do
             line="$q"
-            for e in cqels csparql "querystreamer_type=graphs;interval=false;caching=false"; do
+            for e in "querystreamer_type=graphs;interval=false;caching=false" csparql cqels; do
                 file="$dir/$e/r=1.0;f=$f;dup=$d;q=[$q.txt].csv"
                 e=$(tail -1 $dir/$e/r\=1.0\;f\=$f\;dup\=$d\;q\=\[$q.txt\].csv | awk -F',' '{print $8}')
                 if [ "$e" == "null" ] || [ "$e" == "" ]; then
